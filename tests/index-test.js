93466be4fc562847540af76c280b5d91a8a5d3d2
Tarik Huber <huber.tarik@gmail.com>
2018-06-27 15:00:57 +0200

    Added basic test

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,6 +1,6 @@
 import expect from 'expect'
 import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 
 import Component from 'src/'
 
@@ -16,8 +16,8 @@ describe('Component', () => {
   })
 
   it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+    render(<Component />, node, () => {
+      expect(node.innerHTML).toContain('combobox')
     })
   })
 })
