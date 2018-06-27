2c085563b7dc97ec36d049c3917022952906e2af
Tarik Huber <huber.tarik@gmail.com>
2018-06-27 15:58:03 +0200

    Added AutoSizer to the virtualized SelectField

--- src/VirtualizedSelectField.js
+++ src/VirtualizedSelectField.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import SelectField from './SelectField'
-import { List } from 'react-virtualized'
+import { List, AutoSizer } from 'react-virtualized'
 
 const renderMenu = ({ classes, filteredItems, renderSuggestion, getItemProps, highlightedIndex, selectedItem, itemToString }) => {
-  return <List
-    width={230}
-    scrollToIndex={highlightedIndex || 0}
-    height={200}
-    rowCount={filteredItems.length}
-    rowHeight={48}
-    rowRenderer={
-      ({ key, index, style }) => <div
-        key={key}
-        component='div'
-        style={{ ...style }}
-      >
-        {renderSuggestion({
-          suggestion: filteredItems[index],
-          index,
-          itemProps: getItemProps({ item: filteredItems[index] }),
-          highlightedIndex,
-          selectedItem,
-          itemToString
-        })}
-      </div>
-    }
+  return <AutoSizer>{({ width }) => (
+    <List
+      width={width}
+      scrollToIndex={highlightedIndex || 0}
+      height={200}
+      rowCount={filteredItems.length}
+      rowHeight={48}
+      rowRenderer={
+        ({ key, index, style }) => <div
+          key={key}
+          component='div'
+          style={{ ...style }}
+        >
+          {renderSuggestion({
+            suggestion: filteredItems[index],
+            index,
+            itemProps: getItemProps({ item: filteredItems[index] }),
+            highlightedIndex,
+            selectedItem,
+            itemToString
+          })}
+        </div>
+      }
 
-  />
+    />)}
+  </AutoSizer>
 }
 
 const VirtualizedSelectField = (props) => {
