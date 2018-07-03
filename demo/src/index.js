9795a8bf792cb3ebf7e58f933e046a07472fec02
Tarik Huber <huber.tarik@gmail.com>
2018-07-03 14:50:13 +0200

    Added dynamic height and rowHeight to VirtualSelectField

--- demo/src/index.js
+++ demo/src/index.js
@@ -9,40 +9,40 @@ import { render } from 'react-dom'
 class Demo extends Component {
 
 
-    render() {
+  render() {
 
-        return <div>
-            <h1>muishift Demo</h1>
-            <div style={{ maxWidth: 250 }}>
-                <MuiShift
-                    items={smallList}
-                    itemToString={item => item ? item.label : ''}
-                    inputProps={{ label: 'label', placeholder: 'placeholder', helperText: 'helperText', fullWidth: false }}
-                    onChange={(e) => { console.log(e) }}
-                //input={{ value: { label: 'Afganistan' } }}
-                />
-                <br />
-                <Autocomplete
-                    items={smallList}
-                    itemToString={item => item ? item.label : ''}
-                />
-                <br />
-                <SelectField
-                    items={smallList}
-                    itemToString={item => item ? item.label : ''}
-                    inputProps={{ label: 'label', placeholder: 'placeholder', helperText: 'helperText', disabled: true, fullWidth: false }}
-                />
-                <br />
-                <VirtualizedSelectField
-                    items={bigList}
-                    input={{ value: { code: 'af', name: 'Afghanistan' } }}
-                    itemToString={item => item ? item.name : ''}
-                />
-                <br />
-            </div>
+    return <div>
+      <h1>muishift Demo</h1>
+      <div style={{ maxWidth: 250 }}>
+        <MuiShift
+          items={smallList}
+          itemToString={item => item ? item.label : ''}
+          inputProps={{ label: 'label', placeholder: 'placeholder', helperText: 'helperText', fullWidth: false }}
+          onChange={(e) => { console.log(e) }}
+        //input={{ value: { label: 'Afganistan' } }}
+        />
+        <br />
+        <Autocomplete
+          items={smallList}
+          itemToString={item => item ? item.label : ''}
+        />
+        <br />
+        <SelectField
+          items={smallList}
+          itemToString={item => item ? item.label : ''}
+          inputProps={{ label: 'label', placeholder: 'placeholder', helperText: 'helperText', disabled: true, fullWidth: false }}
+        />
+        <br />
+        <VirtualizedSelectField
+          items={bigList}
+          //input={{ value: { code: 'af', name: 'Afghanistan' } }}
+          itemToString={item => item ? item.name : ''}
+        />
+        <br />
+      </div>
 
-        </div>
-    }
+    </div>
+  }
 }
 
 render(<Demo />, document.querySelector('#demo'))
--- src/VirtualizedSelectField.js
+++ src/VirtualizedSelectField.js
@@ -2,37 +2,47 @@ import React from 'react'
 import SelectField from './SelectField'
 import { List, AutoSizer } from 'react-virtualized'
 
-const renderMenu = ({ rootProps, downshiftProps, filteredItems }) => {
-  const { renderSuggestion } = rootProps
-  const { highlightedIndex } = downshiftProps
-
-  return <AutoSizer>{({ width }) => (
-    <List
-      width={width}
-      scrollToIndex={highlightedIndex || 0}
-      height={200}
-      rowCount={filteredItems.length}
-      rowHeight={48}
-      rowRenderer={
-        ({ key, index, style }) => <div
-          key={key}
-          style={{ ...style }}
-        >
-          {renderSuggestion({
-            rootProps,
-            downshiftProps,
-            suggestion: filteredItems[index],
-            index
-          })}
-        </div>
-      }
-
-    />)}
-  </AutoSizer>
-}
 
 const VirtualizedSelectField = (props) => {
+
+  const renderMenu = ({ rootProps, downshiftProps, filteredItems }) => {
+    const { height, rowHeight } = props
+    const { renderSuggestion } = rootProps
+    const { highlightedIndex } = downshiftProps
+
+    return <AutoSizer>{({ width }) => (
+      <List
+        width={width}
+        scrollToIndex={highlightedIndex || 0}
+        height={height}
+        rowCount={filteredItems.length}
+        rowHeight={rowHeight}
+        rowRenderer={
+          ({ key, index, style }) => <div
+            key={key}
+            style={{ ...style }}
+          >
+            {renderSuggestion({
+              rootProps,
+              downshiftProps,
+              suggestion: filteredItems[index],
+              index
+            })}
+          </div>
+        }
+
+      />)}
+    </AutoSizer>
+  }
+
+
   return <SelectField renderMenu={renderMenu} {...props} />
+}
+
+
+VirtualizedSelectField.defaultProps = {
+  height: 200,
+  rowHeight: 48
 }
 
 export default VirtualizedSelectField
