5af3521cbedc4736e173cf616dac315d8cfaddc0
Tarik Huber <tarik.huber@ics.local>
2019-08-21 09:22:13 +0200

    Fixed MUI spacing unit error

--- src/MuiShift.js
+++ src/MuiShift.js
@@ -129,13 +129,13 @@ const styles = theme => ({
   },
   paper: {
     position: 'absolute',
-    marginTop: theme.spacing.unit,
+    marginTop: theme.spacing(1),
     left: 0,
     right: 0,
     zIndex: zIndex.modal
   },
   chip: {
-    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
+    margin: `${theme.spacing(1) / 2}px ${theme.spacing(1) / 4}px`
   },
   closeButton: {
     '&:hover': {
